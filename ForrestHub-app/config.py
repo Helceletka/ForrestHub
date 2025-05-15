7c4945beec839570bc0b8462eee2429a8a475088
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-05-15 18:02:23 +0200

    Set DEBUG to False for production mode

--- ForrestHub-app/config.py
+++ ForrestHub-app/config.py
@@ -34,7 +34,7 @@ class Config:
     ASSETS_DIR = "assets"
     PAGES_DIR = "pages"
 
-    DEBUG = True
+    DEBUG = False
     USE_RELOADER = False
 
     # use live data if it exists
@@ -50,6 +50,6 @@ class Config:
     TEMPLATES_AUTO_RELOAD = True
 
     # Disable debug and reloader when using frozen data with live data - production mode
-    if not FROZEN:
-        # DEBUG = False
-        USE_RELOADER = True
+    # if not FROZEN:
+    #     # DEBUG = False
+    #     USE_RELOADER = True
